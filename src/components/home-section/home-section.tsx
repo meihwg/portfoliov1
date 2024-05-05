9992b2699d31ae8a997e1fdc11d468a1e57f2feb
Mei <belgrandlaureen.tg2@gmail.com>
2024-05-05 19:14:08 +0200

    fix home page

--- src/components/home-section/home-section.tsx
+++ src/components/home-section/home-section.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Icon } from '@iconify/react';
 import './home-section.css';
 import DesignButton from '../design-button/design-button.tsx';
 
@@ -6,14 +7,24 @@ const HomeSection: React.FC = () => {
     return (
         <>
             <div className="homesection-container">
-                <h1>Laureen Belgrand</h1>
-                <h2>Développeuse Full stack</h2>
+                <div className="home-banner">
+                    <h1>Laureen Belgrand</h1>
+                    <h2>Développeuse Full stack</h2>
+                </div>
                 <div className="buttons-container">
                     <DesignButton buttonLink='#a-propos' buttonText='A propos' buttonType='A' />
                     <DesignButton buttonLink='#competences' buttonText='Compétences' buttonType='B' />
                     <DesignButton buttonLink='#projets' buttonText='Projets' buttonType='C' />
                     <DesignButton buttonLink='#parcours' buttonText='Parcours' buttonType='D' />
                 </div>
+                <nav className="home-nav">
+                    <ul className="home-icons">
+                        <li title="mail : belgrandlaureen.tg2@gmail.com"><a href="mailto: belgrandlaureen.tg2@gmail.com"><Icon icon="material-symbols:mail" className="home-icon type--A" /></a></li>
+                        <li title="tel : 0768735193"><Icon icon="ic:sharp-phone" className="home-icon type--B" /></li>
+                        <li title="LinkedIn - Laureen Belgrand"><a href="https://www.linkedin.com/in/laureen-belgrand-313652208/" target="_blank" rel="noreferrer" className="home-icon type--C"><Icon icon="mdi:linkedin" /></a></li>
+                        <li title="Github - Meihwg"><a href="https://www.github.com/meihwg" target="_blank" rel="noreferrer" className="home-icon type--D"><Icon icon="ant-design:github-filled" /></a></li>
+                    </ul>
+                </nav>
             </div>
         </>
         
--- src/components/slide-navbar/slide-navbar.tsx
+++ src/components/slide-navbar/slide-navbar.tsx
@@ -14,10 +14,10 @@ const SlideNavbar: React.FC = () => {
                 <li className="type--E"><a href="#parcours" data-text="Parcours">Parcours</a></li>
             </ul>
             <ul className="navbar-icons">
-                <li title="mail : belgrandlaureen.tg2@gmail.com"><a href="mailto: belgrandlaureen.tg2@gmail.com"><Icon icon="material-symbols:mail" className="navbar-icon" /></a></li>
-                <li title="tel : 0768735193"><Icon icon="ic:sharp-phone" className="navbar-icon" /></li>
-                <li title="LinkedIn - Laureen Belgrand"><a href="https://www.linkedin.com/in/laureen-belgrand-313652208/" target="_blank" rel="noreferrer" className="navbar-icon"><Icon icon="mdi:linkedin" /></a></li>
-                <li title="Github - Meihwg"><a href="https://www.github.com/meihwg" target="_blank" rel="noreferrer" className="navbar-icon"><Icon icon="ant-design:github-filled" /></a></li>
+                <li title="mail : belgrandlaureen.tg2@gmail.com"><a href="mailto: belgrandlaureen.tg2@gmail.com"><Icon icon="material-symbols:mail" className="navbar-icon type--B" /></a></li>
+                <li title="tel : 0768735193"><Icon icon="ic:sharp-phone" className="navbar-icon type--C" /></li>
+                <li title="LinkedIn - Laureen Belgrand"><a href="https://www.linkedin.com/in/laureen-belgrand-313652208/" target="_blank" rel="noreferrer" className="navbar-icon type--D"><Icon icon="mdi:linkedin" /></a></li>
+                <li title="Github - Meihwg"><a href="https://www.github.com/meihwg" target="_blank" rel="noreferrer" className="navbar-icon type--E"><Icon icon="ant-design:github-filled" /></a></li>
             </ul>
         </nav>
     );
