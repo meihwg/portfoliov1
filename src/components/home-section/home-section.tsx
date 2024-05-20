577df6e921341cc8f6f45d944a037823b523d61f
Mei <belgrandlaureen.tg2@gmail.com>
2024-05-20 14:27:10 +0200

    fix responsive home section et hover

--- src/components/home-section/home-section.tsx
+++ src/components/home-section/home-section.tsx
@@ -5,29 +5,27 @@ import DesignButton from '../design-button/design-button.tsx';
 
 const HomeSection: React.FC = () => {
     return (
-        <>
-            <div className="homesection-container">
-                <div className="home-banner">
-                    <h1>Laureen Belgrand</h1>
-                    <h2>Développeuse Full stack</h2>
-                </div>
-                <div className="buttons-container">
-                    <DesignButton buttonLink='#a-propos' buttonText='A propos' buttonType='A' />
-                    <DesignButton buttonLink='#competences' buttonText='Compétences' buttonType='B' />
-                    <DesignButton buttonLink='#projets' buttonText='Projets' buttonType='C' />
-                    <DesignButton buttonLink='#parcours' buttonText='Parcours' buttonType='D' />
-                </div>
-                <nav className="home-nav">
-                    <ul className="home-icons">
-                        <li title="mail : belgrandlaureen.tg2@gmail.com"><a href="mailto: belgrandlaureen.tg2@gmail.com"><Icon icon="material-symbols:mail" className="home-icon type--A" /></a></li>
-                        <li title="tel : 0768735193"><Icon icon="ic:sharp-phone" className="home-icon type--B" /></li>
-                        <li title="LinkedIn - Laureen Belgrand"><a href="https://www.linkedin.com/in/laureen-belgrand-313652208/" target="_blank" rel="noreferrer" className="home-icon type--C"><Icon icon="mdi:linkedin" /></a></li>
-                        <li title="Github - Meihwg"><a href="https://www.github.com/meihwg" target="_blank" rel="noreferrer" className="home-icon type--D"><Icon icon="ant-design:github-filled" /></a></li>
-                    </ul>
-                </nav>
+        <div className="homesection-container">
+            <div className="home-banner">
+                <h1>Laureen Belgrand</h1>
+                <h2>Développeuse Full stack</h2>
             </div>
-        </>
-        
+            <div className="buttons-container">
+                <DesignButton buttonLink='#a-propos' buttonText='A propos' buttonType='A' />
+                <DesignButton buttonLink='#competences' buttonText='Compétences' buttonType='B' />
+                <DesignButton buttonLink='#projets' buttonText='Projets' buttonType='C' />
+                <DesignButton buttonLink='#parcours' buttonText='Parcours' buttonType='D' />
+            </div>
+            <nav className="home-nav">
+                <ul className="home-icons">
+                    <li title="mail : belgrandlaureen.tg2@gmail.com"><a href="mailto: belgrandlaureen.tg2@gmail.com"><Icon icon="material-symbols:mail" className="home-icon type--A" /></a></li>
+                    <li title="tel : 0768735193"><Icon icon="ic:sharp-phone" className="home-icon type--B" /></li>
+                    <li title="LinkedIn - Laureen Belgrand"><a href="https://www.linkedin.com/in/laureen-belgrand-313652208/" target="_blank" rel="noreferrer" className="home-icon type--C"><Icon icon="mdi:linkedin" /></a></li>
+                    <li title="Github - Meihwg"><a href="https://www.github.com/meihwg" target="_blank" rel="noreferrer" className="home-icon type--D"><Icon icon="ant-design:github-filled" /></a></li>
+                </ul>
+            </nav>
+        </div>
+
     );
 }
 
--- src/components/slide-navbar/slide-navbar.tsx
+++ src/components/slide-navbar/slide-navbar.tsx
@@ -30,23 +30,25 @@ const SlideNavbar: React.FC = () => {
             </button>
             {
                 navbarToggle ?
-                <div className="navbar-hide" onClick={toggleNavbar}>
-                    <nav className="navbar">
-                        <ul className="menu-hover-fill">
-                            <li className="type--A"><a href="#accueil" data-text="Accueil" onClick={toggleNavbar}>Accueil</a></li>
-                            <li className="type--B"><a href="#a-propos" data-text="A propos" onClick={toggleNavbar}>A propos</a></li>
-                            <li className="type--C"><a href="#competences" data-text="Compétences" onClick={toggleNavbar}>Compétences</a></li>
-                            <li className="type--D"><a href="#projets" data-text="Projets" onClick={toggleNavbar}>Projets</a></li>
-                            <li className="type--E"><a href="#parcours" data-text="Parcours" onClick={toggleNavbar}>Parcours</a></li>
-                        </ul>
-                        <ul className="navbar-icons">
-                            <li title="mail : belgrandlaureen.tg2@gmail.com"><a href="mailto: belgrandlaureen.tg2@gmail.com"><Icon icon="material-symbols:mail" className="navbar-icon type--B" /></a></li>
-                            <li title="tel : 0768735193"><Icon icon="ic:sharp-phone" className="navbar-icon type--C" /></li>
-                            <li title="LinkedIn - Laureen Belgrand"><a href="https://www.linkedin.com/in/laureen-belgrand-313652208/" target="_blank" rel="noreferrer" className="navbar-icon type--D"><Icon icon="mdi:linkedin" /></a></li>
-                            <li title="Github - meihwg"><a href="https://www.github.com/meihwg" target="_blank" rel="noreferrer" className="navbar-icon type--E"><Icon icon="ant-design:github-filled" /></a></li>
-                        </ul>
-                    </nav>
-                </div> : null
+                <nav className="navbar" onClick={toggleNavbar}>
+                    {
+                        screenWidth < 868 ? 
+                        <div className="navbar-hide" onClick={toggleNavbar}></div> : null
+                    }
+                    <ul className="menu-hover-fill">
+                        <li className="type--A"><a href="#accueil" data-text="Accueil" onClick={toggleNavbar}>Accueil</a></li>
+                        <li className="type--B"><a href="#a-propos" data-text="A propos" onClick={toggleNavbar}>A propos</a></li>
+                        <li className="type--C"><a href="#competences" data-text="Compétences" onClick={toggleNavbar}>Compétences</a></li>
+                        <li className="type--D"><a href="#projets" data-text="Projets" onClick={toggleNavbar}>Projets</a></li>
+                        <li className="type--E"><a href="#parcours" data-text="Parcours" onClick={toggleNavbar}>Parcours</a></li>
+                    </ul>
+                    <ul className="navbar-icons">
+                        <li title="mail : belgrandlaureen.tg2@gmail.com"><a href="mailto: belgrandlaureen.tg2@gmail.com"><Icon icon="material-symbols:mail" className="navbar-icon type--B" /></a></li>
+                        <li title="tel : 0768735193"><Icon icon="ic:sharp-phone" className="navbar-icon type--C" /></li>
+                        <li title="LinkedIn - Laureen Belgrand"><a href="https://www.linkedin.com/in/laureen-belgrand-313652208/" target="_blank" rel="noreferrer" className="navbar-icon type--D"><Icon icon="mdi:linkedin" /></a></li>
+                        <li title="Github - meihwg"><a href="https://www.github.com/meihwg" target="_blank" rel="noreferrer" className="navbar-icon type--E"><Icon icon="ant-design:github-filled" /></a></li>
+                    </ul>
+                </nav> : null
             }
             
         </>
